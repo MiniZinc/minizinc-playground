fd97df36032cb4bd1711737b6d70186a2b235f05
Automatic library publish <>
2023-12-14 22:16:17 +0000

    Publish library (5d14c98dd20d2bbd2b7b438fe98d386d327d7267)

---
+++ dist/minizinc-playground.js
@@ -0,0 +1,6 @@
+import { P as p } from "./embed-1XjuZIqh.js";
+import "https://cdn.jsdelivr.net/npm/minizinc/dist/minizinc.mjs";
+import "https://cdn.jsdelivr.net/npm/minizinc@edge/dist/minizinc.mjs";
+export {
+  p as default
+};
